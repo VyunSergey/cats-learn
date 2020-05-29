a71c5bc7c087d8c788b30fdf0ad1b4d4789e72a9
Vyun Sergey <vyun.sergey@icloud.com>
2020-05-29 10:43:26 +0300

    add monoid syntax

--- src/main/scala/monoids/MonoidTest.scala
+++ src/main/scala/monoids/MonoidTest.scala
@@ -1,6 +1,7 @@
 package monoids
 
 import cats.Monoid
+import cats.syntax.monoid._
 import MonoidLaws._
 
 object MonoidTest extends App {
@@ -13,13 +14,17 @@ object MonoidTest extends App {
   println(s"j: $j")
   println(s"k: $k")
   println("i + j: " + mInt.combine(i, j))
+  println("i |+| j: " + (i |+| j))
   println("(i + j) + k: " + mInt.combine(mInt.combine(i, j), k))
   println("i + (j + k): " + mInt.combine(i, mInt.combine(j, k)))
+  println("i |+| j |+| k: " + (i |+| j |+| k))
 
   println("check Associative Law for Int: " + associativeLaw(i, j, k))
 
   println(s"empty: ${mInt.empty}")
+  println(s"Is empty: ${mInt.empty.isEmpty}")
   println("i + empty: " + mInt.combine(i, mInt.empty))
+  println("i |+| empty: " + (i |+| mInt.empty))
 
   println("check Identity Law for Int: " + identityLaw(i))
 
@@ -29,6 +34,7 @@ object MonoidTest extends App {
   val mString: Monoid[String] = Monoid[String]
 
   println(mString.combine(mString.combine(s1, s2), s3))
+  println(s1 |+| s2 |+| s3)
   println(List(s1, s2, s3).foldLeft(mString.empty)(mString.combine))
   println(List(s1, s2, s3).reduce(mString.combine))
 
