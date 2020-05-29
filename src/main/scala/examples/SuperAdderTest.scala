013801a7565b7f51c1e4721fb9e19f4dff306f3f
Vyun Sergey <vyun.sergey@icloud.com>
2020-05-29 11:18:39 +0300

    add SuperAdder for String

--- src/main/scala/examples/SuperAdderTest.scala
+++ src/main/scala/examples/SuperAdderTest.scala
@@ -6,9 +6,11 @@ object SuperAdderTest extends App {
   val lst: List[Int] = List(1, 2, 3, 4)
   val opLst: List[Option[Int]] = lst.map(Option(_))
   val opLstWithNone: List[Option[Int]] = opLst :+ None
+  val strLst: List[String] = opLstWithNone.map(_.fold("")(_.toString))
 
   println(s"add [${lst.mkString(", ")}]: " + SuperAdder[Int].add(lst))
   println(s"add [${opLst.mkString(", ")}]: " + SuperAdder[Option[Int]].add(opLst))
   println(s"add [${opLstWithNone.mkString(", ")}]: " + SuperAdder[Option[Int]].add(opLstWithNone))
+  println(s"add [${strLst.mkString(", ")}]: " + SuperAdder[String].add(strLst))
 
 }
