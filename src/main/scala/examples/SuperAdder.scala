ed26685fb70eeffe6f81b803ffab3676f3550d84
Vyun Sergey <vyun.sergey@icloud.com>
2020-05-29 11:12:30 +0300

    add SuperAdder example

---
+++ src/main/scala/examples/SuperAdder.scala
@@ -0,0 +1,13 @@
+package examples
+
+import cats.Monoid
+
+trait SuperAdder[A] {
+  def add(items: List[A])(implicit m: Monoid[A]): A = {
+    items.foldLeft(m.empty)(m.combine)
+  }
+}
+
+object SuperAdder {
+  def apply[A](implicit m: Monoid[A]): SuperAdder[A] = new SuperAdder[A] {}
+}
---
+++ src/main/scala/examples/SuperAdderTest.scala
@@ -0,0 +1,14 @@
+package examples
+
+import cats.instances.int._
+
+object SuperAdderTest extends App {
+  val lst: List[Int] = List(1, 2, 3, 4)
+  val opLst: List[Option[Int]] = lst.map(Option(_))
+  val opLstWithNone: List[Option[Int]] = opLst :+ None
+
+  println(s"add [${lst.mkString(", ")}]: " + SuperAdder[Int].add(lst))
+  println(s"add [${opLst.mkString(", ")}]: " + SuperAdder[Option[Int]].add(opLst))
+  println(s"add [${opLstWithNone.mkString(", ")}]: " + SuperAdder[Option[Int]].add(opLstWithNone))
+
+}
