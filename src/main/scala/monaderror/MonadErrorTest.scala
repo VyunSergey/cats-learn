d0dbf4590718bc42cfb7c8b519eadf0fbde045a1
SberHackathonTeamKVN <treamkvn@yandex.ru>
2020-06-03 16:19:52 +0300

    add syntax in MonadErrorTest

--- src/main/scala/monaderror/MonadErrorTest.scala
+++ src/main/scala/monaderror/MonadErrorTest.scala
@@ -2,6 +2,9 @@ package monaderror
 
 import cats.MonadError
 import cats.instances.either._
+import cats.syntax.applicative._
+import cats.syntax.applicativeError._
+import cats.syntax.monadError._
 
 object MonadErrorTest extends App {
   type ErrorOr[A] = Either[String, A]
@@ -9,17 +12,29 @@ object MonadErrorTest extends App {
   val monadError: MonadError[ErrorOr, String] = MonadError[ErrorOr, String]
 
   val success: ErrorOr[Int] = monadError.pure(123)
+  val success2: ErrorOr[Int] = 123.pure
   println(success)
+  println(success2)
+
   val failure: ErrorOr[Int] = monadError.raiseError("Error")
+  val failure2: ErrorOr[Int] = "Error".raiseError[ErrorOr, Int]
   println(failure)
+  println(failure2)
 
   val handled: ErrorOr[Int] = monadError.handleError(failure) {
     case "Error" => 1
     case _ => 0
   }
+  val handled2: ErrorOr[Int] = failure.handleError {
+    case "Error" => 1
+    case _ => 0
+  }
   println(handled)
+  println(handled2)
 
   val ensured: ErrorOr[Int] = monadError.ensure(success)("Value is < 1000")(_ < 1000)
+  val ensured2: ErrorOr[Int] = success.ensure("Value is < 1000")(_ < 1000)
   println(ensured)
+  println(ensured2)
 
 }
